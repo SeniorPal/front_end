d50c25fcaa5d06d512a502f1203c5e0219e7a71b
Wendu Sha <103120467+Edevernot@users.noreply.github.com>
2024-05-07 12:46:11 +0800

    Add files via upload
    应用后台状态显示服务（虽然没什么卵用，本意是修复语音唤醒后偶尔才能够打开应用主界面的bug，没修复成功，怀疑还是与安卓的后台进程管理有关）

---
+++ app/src/main/java/com/project/seniorpal/ForegroundService.java
@@ -0,0 +1,68 @@
+package com.project.seniorpal;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import androidx.core.app.NotificationCompat;
+
+public class ForegroundService extends Service {
+    private static final int NOTIFICATION_ID = 1;
+    private static final String CHANNEL_ID = "ForegroundServiceChannel";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel();
+    }
+
+    @SuppressLint("ForegroundServiceType")
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Notification notification = getNotification(); // 确保这个方法返回一个有效的通知对象
+        startForeground(NOTIFICATION_ID, notification);
+        return START_NOT_STICKY;
+    }
+
+
+    private Notification getNotification() {
+        NotificationChannel channel = new NotificationChannel("channel_01", "Channel Name", NotificationManager.IMPORTANCE_DEFAULT);
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+
+        Notification.Builder builder = new Notification.Builder(this, "channel_01")
+                .setContentTitle("Service Running")
+                .setContentText("This is a running foreground service")
+                .setSmallIcon(R.drawable.ic_banner_foreground);
+        return builder.build();
+    }
+
+
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Foreground Service";
+            String description = "用于保持应用后台运行";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopForeground(true);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null; // 用于绑定，不需要时返回null
+    }
+}
